b09e8a7c32da74fa3045cf0a16245a1957e89181
Maria Thompson <marialouise1216@gmail.com>
2025-07-17 20:21:21 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Maria Thompson on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
